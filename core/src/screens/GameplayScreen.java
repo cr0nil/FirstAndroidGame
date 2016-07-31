ff37897fff7eba2fcc71de705651a132b924dc9c
cr0nil <karlkurt1996@gamil.com>
2016-07-31 13:51:12 +0200

    add palyer button

--- core/src/screens/GameplayScreen.java
+++ core/src/screens/GameplayScreen.java
@@ -1,6 +1,9 @@
 package screens;
 
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.firstx.cos.Piersi;
 
 import entities.Jumper;
@@ -11,6 +14,7 @@ import entities.Jumper;
 public class GameplayScreen extends AbstractScreen {
 
     private Jumper jumper;
+    private Button playerButton;
 
     public GameplayScreen(Piersi game) {
         super(game);
@@ -19,8 +23,26 @@ public class GameplayScreen extends AbstractScreen {
     @Override
     protected void init() {
         initPlayer();
+        initPlayerButton();
     }
 
+    private void initPlayerButton() {
+        playerButton = new Button(new Button.ButtonStyle());
+        playerButton.setWidth(460);
+        playerButton.setHeight(360);
+        playerButton.setX(100);
+        playerButton.setY(100);
+        playerButton.setDebug(true);
+        stage.addActor(playerButton);
+
+        playerButton.addListener(new ClickListener() {
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                return super.touchDown(event, x, y, pointer, button);
+            }
+        });
+    }
     private void initPlayer() {
         jumper = new Jumper();
         stage.addActor(jumper);
