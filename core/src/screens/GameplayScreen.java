5b3c3cd6a91585c66f52bc2bc330cc86d2ea08eb
cr0nil <karlkurt1996@gamil.com>
2016-08-01 18:05:22 +0200

    no message

--- core/src/screens/GameplayScreen.java
+++ core/src/screens/GameplayScreen.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.firstx.cos.Piersi;
 
