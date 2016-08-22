68cf475dca0d6df8e085c3954bb30d401ed0b279
cr0nil <karlkurt1996@gamil.com>
2016-08-22 22:20:43 +0200

    #1 add menu screen

---
+++ core/src/screens/MenuScreen.java
@@ -0,0 +1,55 @@
+package screens;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.firstx.cos.Piersi;
+
+import Controllers.GameScreenController;
+import UserInterface_UI.IClickCallback;
+import UserInterface_UI.PlayButton;
+
+/**
+ * Created by Karol on 22.08.2016.
+ */
+public class MenuScreen extends AbstractScreen {
+    private PlayButton playButton;
+    private Texture background;
+    public MenuScreen(GameScreenController gsc) {
+        super(gsc);
+        background = new Texture("123.png");
+
+    }
+    @Override
+    public void init(){
+        initPlayButton();
+    }
+    @Override
+    public void handleInput() {
+
+    }
+    private void initPlayButton() {
+        playButton = new PlayButton(new IClickCallback() {
+            @Override
+            public void onClick() {
+                game.setScreen(new GameplayScreen(game));
+//                game.getScoreService().restGameScore();
+            }
+        });
+        stage.addActor(playButton);
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(background,0,0, Piersi.WIDTH,Piersi.HEIGHT);
+        sb.end();
+
+    }
+
+
+}
